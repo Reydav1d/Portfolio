fc6474d7413e68b16a453da66e3aa6914fd0e0af
David Perez Tiburcio <sewer7375@gmail.com>
2023-09-08 12:07:49 -0600

    add: labels and styles in contactForm

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,14 +3,36 @@ import React from "react";
 import SocialCard from "@/components/socialCard/socialCard";
 import { useForm, ValidationError } from "@formspree/react";
 import Link from "next/link";
+import { Button } from "flowbite-react";
 
 export default function Contact() {
   const [state, handleSubmit] = useForm("xaygjkvk");
   if (state.succeeded) {
     return (
       <>
-        <p>¡Correo enviado exitosamente! Espera una respuesta muy pronto.</p>
-        <Link href="/">Volver al inicio</Link>
+        <div className="h-screen flex flex-col justify-center items-center space-y-8">
+          <h1 className="text-3xl font-extrabold dark:text-white">
+            ¡Correo enviado exitosamente! Espera una respuesta muy pronto.
+          </h1>
+          <svg
+            className="w-[48px] h-[48px] text-cyan-900"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="M4.008 8.714c1-.097 1.96-.45 2.792-1.028a25.112 25.112 0 0 0 4.454-5.72 1.8 1.8 0 0 1 .654-.706 1.742 1.742 0 0 1 1.65-.098 1.82 1.82 0 0 1 .97 1.128c.075.248.097.51.065.767l-1.562 4.629M4.008 8.714H1v9.257c0 .273.106.535.294.728a.99.99 0 0 0 .709.301h1.002a.99.99 0 0 0 .71-.301c.187-.193.293-.455.293-.728V8.714Zm8.02-1.028h4.968c.322 0 .64.08.925.232.286.153.531.374.716.645a2.108 2.108 0 0 1 .242 1.883l-2.36 7.2c-.288.813-.48 1.354-1.884 1.354-2.59 0-5.39-1.06-7.504-1.66"
+            />
+          </svg>
+          <Link href="/">
+            <Button>Volver al inicio</Button>
+          </Link>
+        </div>
       </>
     );
   }
@@ -59,10 +81,26 @@ export default function Contact() {
               placeholder="Escribe el asunto de tu correo"
             />
             <ValidationError
-              prefix="Email"
-              field="email"
+              prefix="Asunto"
+              field="asunto"
               errors={state.errors}
             />
+          </div>
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-cyan-600"
+            >
+              Nombre
+            </label>
+            <input
+              id="name"
+              type="name"
+              name="name"
+              className="shadow-sm bg-gray-300 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+              placeholder="¿Cuál es tu nombre?"
+            />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
           </div>
           <div>
             <label
